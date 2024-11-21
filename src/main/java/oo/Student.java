137f26181f0bec0db4374a97275fb1b0427d0b76
Sylas Qiu <qiusy@oocl.com>
2024-11-21 13:41:39 +0800

    [OOP] feat: step2 student extends person

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,12 @@
 package oo;
 
-public class Student {
+public class Student extends Person {
+    public Student(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a student.";
+    }
 }
