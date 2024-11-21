574b52e793cc5123cd5ccc8f45b398e3a4a39621
Sylas Qiu <qiusy@oocl.com>
2024-11-21 14:00:14 +0800

    [OOP] feat: step3 add isIn method for student

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,6 +1,9 @@
 package oo;
 
 public class Student extends Person {
+
+    private Klass klass;
+
     public Student(Integer id, String name, Integer age) {
         super(id, name, age);
     }
@@ -8,5 +11,9 @@ public class Student extends Person {
     @Override
     public String introduce() {
         return super.introduce() + " I am a student.";
+    }
+
+    public boolean isIn(Klass klass){
+        return this.klass == klass;
     }
 }
