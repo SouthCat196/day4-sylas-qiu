f5644d4eb8bd2f61ae394c1fe2fd66883a687312
Sylas Qiu <qiusy@oocl.com>
2024-11-21 13:37:39 +0800

    [OOP] feat: step1 add id name age prop and introduce method for person

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,17 @@
 package oo;
 
 public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
 }
