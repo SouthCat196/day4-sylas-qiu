9248dfb41a5df68a1842edf6ab91f6cc11db0c53
Sylas Qiu <qiusy@oocl.com>
2024-11-21 14:09:02 +0800

    [OOP] feat: step4 add belongsTo method for teacher

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,12 +1,24 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher extends Person {
+
+    private List<Klass> klasses;
+
     public Teacher(Integer id, String name, Integer age) {
         super(id, name, age);
+        klasses = new ArrayList<>();
     }
 
     @Override
     public String introduce() {
         return super.introduce() + " I am a teacher.";
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return klasses.stream()
+                .anyMatch(teachingKlass -> teachingKlass == klass);
     }
 }
