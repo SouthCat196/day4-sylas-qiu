2d7ce03fdc326d7501f1347937e0dd855f0c27ea
Sylas Qiu <qiusy@oocl.com>
2024-11-21 13:56:22 +0800

    [OOP] feat: step3 add number prop for klass and override equals and hashCode

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,25 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return Objects.equals(number, klass.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
 }
